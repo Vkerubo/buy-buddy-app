1c3b810174e2094e63d1f3ed0c45d31947b31157
sandra <nyamweasandra@gmail.com>
2023-05-04 15:59:09 +0300

    return

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -5,3 +5,16 @@ import { CartContext } from '../contexts/CartContext';
 function ShoppingCart() {
     const { cart } = useContext(CartContext); 
     // Use useContext
+    return (
+        <div>
+          {cart.map((product) => ( // Render shopping cart items using map function
+            <div key={product.id}>
+              <h3>{product.title}</h3> // Render product title
+              <p>{product.price}</p> // Render product price
+            </div>
+          ))}
+        </div>
+      );
+    }
+    
+    export default ShoppingCart; 
