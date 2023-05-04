7ec23292f0f2228b0b193aed73328e2d877b5727
Valentine <kerubovalentine1@gmail.com>
2023-05-04 20:10:40 +0300

    save CartContext

--- src/Contexts/CartContext.js
+++ src/Contexts/CartContext.js
@@ -9,6 +9,19 @@ const CartContextProvider = (props) => {
     // Use the setCart function to update the "cart" state by adding the new product to the array
     setCart([...cart, product]);
   };
+
+  const removeFromCart = (product) => {
+    // Use the filter() method to create a new array called "updatedCart" that excludes the product with the matching ID
+    const updatedCart = cart.filter((item) => item.id !== product.id);
+    // Use the setCart function to update the "cart" state with the new "updatedCart" array
+    setCart(updatedCart);
+  };
+  // Use the CartContext.Provider component to provide the "cart", "addToCart", and "removeFromCart" values to any child components that need them
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartContextProvider;
