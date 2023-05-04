7e227fda4aa183f8fbfdfedeef960065417a0baa
sandra <nyamweasandra@gmail.com>
2023-05-04 15:47:23 +0300

    fetch

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -2,3 +2,18 @@ import { useState, useEffect } from 'react';
 // Import useState and useEffect hooks from React
 import { Link } from 'react-router-dom'; 
 // Import Link component from react-router-dom package
+function ProductList() {
+  const [products, setProducts] = useState([]);
+   useEffect(() => { // Use useEffect hook to fetch data when component mounts
+    const fetchProducts = async () => { 
+        // Define asynchronous function to fetch data
+      const response = await fetch('https://fakestoreapi.com/products'); 
+      // Make GET request to API endpoint
+      const data = await response.json(); 
+      // Parse response data to JSON
+      setProducts(data); 
+      // Update state with fetched product data
+    };
+
+    fetchProducts(); // Call fetchProducts function
+  }, []); // Specify empty dependency array to run effect only on mount
