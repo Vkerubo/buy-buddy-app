03b95fb2f2c48e97434a38e37af3999be5287e5a
Valentine <kerubovalentine1@gmail.com>
2023-05-04 20:20:30 +0300

    import and add CartContextProvider

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Home from "./Components/Home";
 import ProductList from "./Components/ProductList";
 import Product from "./Components/Product";
 import ShoppingCart from "./Components/ShoppingCart";
+import CartContextProvider from "./Contexts/CartContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/products" component={ProductList} />
-          <Route exact path="/products/:id" component={Product} />
-          <Route exact path="/cart" component={ShoppingCart} />
-        </Switch>
-      </div>
+      <CartContextProvider>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={ProductList} />
+            <Route exact path="/products/:id" component={Product} />
+            <Route exact path="/cart" component={ShoppingCart} />
+          </Switch>
+        </div>
+      </CartContextProvider>
     </BrowserRouter>
   );
 }
