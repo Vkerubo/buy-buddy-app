d0eaee79f931813366b54d109e7c23898b72ab73
Valentine <kerubovalentine1@gmail.com>
2023-05-04 21:56:10 +0300

    render list of products

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,36 +1,33 @@
 import { useState, useEffect } from "react";
-// Import useState and useEffect hooks from React
 import { Link } from "react-router-dom";
-// Import Link component from react-router-dom package
+
 function ProductList() {
+  // Declare a state variable called "products" and initialize it to an empty array
   const [products, setProducts] = useState([]);
+
+  // Use the useEffect hook to fetch the list of products from the API and update the "products" state variable
   useEffect(() => {
-    // Use useEffect hook to fetch data when component mounts
     const fetchProducts = async () => {
-      // Define asynchronous function to fetch data
       const response = await fetch("https://fakestoreapi.com/products");
-      // Make GET request to API endpoint
       const data = await response.json();
-      // Parse response data to JSON
       setProducts(data);
-      // Update state with fetched product data
     };
-
     fetchProducts();
-    // Call fetchProducts function
-  }, []); // Specify empty dependency array to run effect only on mount
+  }, []);
 
+  // Render the list of products
   return (
     <div>
-      {products.map(
-        (
-          product // Render product list using map function
-        ) => (
-          <div key={product.id}>
-            <Link to={`/products/${product.id}`}>{product.title}</Link>
-          </div>
-        )
-      )}
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <img src={product.image} alt={product.title} />
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          {/* Use the Link component from react-router-dom to create a clickable link that navigates to the individual product page */}
+          <Link to={`/products/${product.id}`}>View Details</Link>
+        </div>
+      ))}
     </div>
   );
 }
