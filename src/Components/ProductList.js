5834d0a6a2d1993c954bfc2fa5a3d6dae343e14e
Valentine <kerubovalentine1@gmail.com>
2023-05-04 22:05:43 +0300

    Add add to cart button

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -15,7 +15,17 @@ function ProductList() {
     fetchProducts();
   }, []);
 
-  // Render the list of products
+  // Define a function called "addToCart" that takes a product object as an argument and adds it to the shopping cart in local storage
+  const addToCart = (product) => {
+    // Check if there is an existing cart in local storage
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || [];
+    // Add the new product to the existing cart
+    const updatedCart = [...existingCart, product];
+    // Store the updated cart in local storage
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+  // Render the list of products with a "View Details" clickable link and an "Add to Cart" button for each product
   return (
     <div>
       {products.map((product) => (
@@ -24,6 +34,8 @@ function ProductList() {
           <img src={product.image} alt={product.title} />
           <p>{product.description}</p>
           <p>Price: ${product.price}</p>
+          {/* Add an "Add to Cart" button that calls the "addToCart" function when clicked */}
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
           {/* Use the Link component from react-router-dom to create a clickable link that navigates to the individual product page */}
           <Link to={`/products/${product.id}`}>View Details</Link>
         </div>
