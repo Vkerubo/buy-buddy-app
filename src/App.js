aa78613dac7a92a00e6783bc9d0368eb909d92c8
Valentine <kerubovalentine1@gmail.com>
2023-05-04 16:07:44 +0300

    import components

--- src/App.js
+++ src/App.js
@@ -1,2 +1,6 @@
-
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import ProductList from "./ProductList";
+import Product from "./Product";
+import ShoppingCart from "./ShoppingCart";
