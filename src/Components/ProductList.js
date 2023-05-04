5390ba8ed1d4613bff7bd439e86a3564971c739b
sandra <nyamweasandra@gmail.com>
2023-05-04 15:44:12 +0300

    link

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,2 +1,4 @@
 import { useState, useEffect } from 'react'; 
 // Import useState and useEffect hooks from React
+import { Link } from 'react-router-dom'; 
+// Import Link component from react-router-dom package
