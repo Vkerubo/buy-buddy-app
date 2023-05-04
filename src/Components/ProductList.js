425274f3bddb1c1e39c7126f039d8a6a13fd9893
Valentine <kerubovalentine1@gmail.com>
2023-05-04 21:44:26 +0300

    modify ProductList

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,34 +1,38 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from "react";
 // Import useState and useEffect hooks from React
-import { Link } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
 // Import Link component from react-router-dom package
 function ProductList() {
   const [products, setProducts] = useState([]);
-   useEffect(() => { // Use useEffect hook to fetch data when component mounts
-    const fetchProducts = async () => { 
-        // Define asynchronous function to fetch data
-      const response = await fetch('https://fakestoreapi.com/products'); 
+  useEffect(() => {
+    // Use useEffect hook to fetch data when component mounts
+    const fetchProducts = async () => {
+      // Define asynchronous function to fetch data
+      const response = await fetch("https://fakestoreapi.com/products");
       // Make GET request to API endpoint
-      const data = await response.json(); 
+      const data = await response.json();
       // Parse response data to JSON
-      setProducts(data); 
+      setProducts(data);
       // Update state with fetched product data
     };
 
     fetchProducts();
-     // Call fetchProducts function
+    // Call fetchProducts function
   }, []); // Specify empty dependency array to run effect only on mount
-   
-    return (
+
+  return (
     <div>
-      {products.map((product) => ( // Render product list using map function
-        <div key={product.id}>
-          <Link to={`/products/${product.id}`}>{product.title}</Link>
-           // Render clickable link to individual product page
-        </div>
-      ))}
+      {products.map(
+        (
+          product // Render product list using map function
+        ) => (
+          <div key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </div>
+        )
+      )}
     </div>
   );
 }
 
-export default ProductList; 
+export default ProductList;
