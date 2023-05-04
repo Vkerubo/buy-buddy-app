7c79ed53e0750311b62f45c79dc0c7c7e3dc0ca9
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-04 15:49:57 +0300

    fix function product

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-     // use Effect
-     
+     // function product
+     function Product() {
+        const [product, setProduct] = useState({});
+        const { id } = useParams();
+      
+        useEffect(() => {
+          const fetchProduct = async () => {
+            const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+            const data = await response.json();
+            setProduct(data);
+          };
+
 
 }
 export default Product;
