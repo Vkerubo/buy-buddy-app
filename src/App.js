5d3ecdf42c078b879e1c24ad2b33fd3d3b2ee1c3
Valentine <kerubovalentine1@gmail.com>
2023-05-04 16:08:12 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -4,3 +4,21 @@ import Home from "./Home";
 import ProductList from "./ProductList";
 import Product from "./Product";
 import ShoppingCart from "./ShoppingCart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={ProductList} />
+          <Route exact path="/products/:id" component={Product} />
+          <Route exact path="/cart" component={ShoppingCart} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
