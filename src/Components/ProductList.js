0970cc868c77a06a90d9a2e3a2cfd0b348e7ad6d
Valentine <kerubovalentine1@gmail.com>
2023-05-05 05:59:39 +0300

    add search bar

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -3,18 +3,43 @@ import Product from "./Product";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data);
+        setFilteredProducts(data);
+      })
       .catch((error) => console.log(error));
   }, []);
 
+  const handleSearch = () => {
+    const filtered = products.filter((product) =>
+      product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <h2>Product List</h2>
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </div>
