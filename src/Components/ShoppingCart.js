fbcd76c62637e7a76a3e57075f304655ecf4464d
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-07 10:19:40 +0300

    modified

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -6,32 +6,9 @@ import "./ShoppingCart.css";
 Modal.setAppElement("#root");
 
 function ShoppingCart() {
-  const { cart, removeFromCart } = useContext(CartContext);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleRemoveFromCart = (product) => {
-    removeFromCart(product);
-  };
-
-  const handleCheckout = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const createCheckoutMessage = () => {
-    const items = cart.map((item) => ({
-      title: item.title,
-      count: item.count - (item.removedCount || 0),
-      price: item.price,
-    }));
-    const total = cart.reduce(
-      (acc, item) => acc + (item.count - (item.removedCount || 0)) * item.price,
-      0
-    );
+    const { cart } = useContext(CartContext); 
+          // use context
     return (
       <div>
         <p>Your Order:</p>
@@ -54,18 +31,12 @@ function ShoppingCart() {
         <p>Your cart is empty.</p>
       ) : (
         <div>
-          <h2>Your Shopping Cart</h2>
-          {cart.map((item) => (
-            <div key={`${item.id}-${item.count}`}>
-              <h3>{item.title}</h3>
-              <img src={item.image} alt={item.title} />
-              <p>Price: ${item.price}</p>
-              <p>
-                Added to cart: {item.count - (item.removedCount || 0)} times
-              </p>
-              <button onClick={() => handleRemoveFromCart(item)}>
-                Remove from Cart
-              </button>
+
+          {cart.map((product) => (
+             // Render shopping cart items using map function
+            <div key={product.id}>
+              <h3>{product.title}</h3> // Render product title
+              <p>{product.price}</p> // Render product price
             </div>
           ))}
           <button className="checkout" onClick={handleCheckout}>
@@ -87,6 +58,5 @@ function ShoppingCart() {
       )}
     </div>
   );
-}
 
 export default ShoppingCart;
